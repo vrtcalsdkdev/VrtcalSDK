6030ccd79b52657be307f8f0e5d69339230ef61e
Scott McCoy <scotthmccoy@gmail.com>
2020-12-29 16:51:15 -0800

    2.0.0-7
    feat: Minor Logging improvements should make integration easier
    fix: crash on SDK init with empty-string serverUrl
    fix: issue where banner video ads on older devices would not resume play when returning to app
    fix: issue where generic device model name was used instead of specific one

--- VrtcalSDK.framework/Headers/VRTLog.h
+++ VrtcalSDK.framework/Headers/VRTLog.h
@@ -17,7 +17,7 @@ typedef NS_ENUM(NSInteger, VRTLogLevel) {
     VRTLogLevelInfo     = 0,    //Debug messages, intended to be read by SDK devs and support team
     VRTLogLevelWarn     = 1,    //Pub-facing messages intended to help diagnose minor issues (lack of fill, misconfiguration)
     VRTLogLevelError    = 2,    //Pub-facing messages intended to help diagnose major issues
-    VRTLogLevelNone     = 3     //Used to squelch all logging messages
+    VRTLogLevelAlways   = 3     //Pub-facing messaged intended to never be squelched
 };
 
 /**
@@ -28,6 +28,7 @@ typedef NS_ENUM(NSInteger, VRTLogLevel) {
 #define VRTLogInfo( s, ... ) [VRTLog log:[NSString stringWithFormat:(s), ##__VA_ARGS__] logLevel:VRTLogLevelInfo file:__FILE__ line:__LINE__ function:__PRETTY_FUNCTION__]
 #define VRTLogWarn( s, ... ) [VRTLog log:[NSString stringWithFormat:(s), ##__VA_ARGS__] logLevel:VRTLogLevelWarn file:__FILE__ line:__LINE__ function:__PRETTY_FUNCTION__]
 #define VRTLogError( s, ... ) [VRTLog log:[NSString stringWithFormat:(s), ##__VA_ARGS__] logLevel:VRTLogLevelError file:__FILE__ line:__LINE__ function:__PRETTY_FUNCTION__]
+#define VRTLogAlways( s, ... ) [VRTLog log:[NSString stringWithFormat:(s), ##__VA_ARGS__] logLevel:VRTLogLevelAlways file:__FILE__ line:__LINE__ function:__PRETTY_FUNCTION__]
 
 @interface VRTLog : NSObject
 
