555e69e20a5dad747e625c5bf05584f64aca9884
Scott McCoy <scott.mccoy@vrtcal.com>
2023-11-13 23:29:06 -0800

    2.1.5.9
    Made Protected Header Private

---
+++ VrtcalSDK.xcframework/ios-arm64/VrtcalSDK.framework/PrivateHeaders/VrtcalSDK_Private.h
@@ -0,0 +1,15 @@
+//
+//  VrtcalSDK
+//
+//  Created by Scott McCoy on 7/3/23.
+//  Copyright © 2023 VRTCAL. All rights reserved.
+//
+
+#import "OMIDAdEvents.h"
+#import "OMIDAdSession.h"
+#import "OMIDAdSessionConfiguration.h"
+#import "OMIDAdSessionContext.h"
+#import "OMIDPartner.h"
+#import "OMIDSDK.h"
+#import "OMIDScriptInjector.h"
+
