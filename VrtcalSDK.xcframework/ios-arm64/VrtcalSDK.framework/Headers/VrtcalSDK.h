60723fbd236c0d03b6508f19dd6482a0f6046e93
Scott McCoy <scott.mccoy@vrtcal.com>
2023-11-13 17:38:36 -0800

    2.1.5.4
    OMSDK Integration Issues

--- VrtcalSDK.xcframework/ios-arm64/VrtcalSDK.framework/Headers/VrtcalSDK.h
+++ VrtcalSDK.xcframework/ios-arm64/VrtcalSDK.framework/Headers/VrtcalSDK.h
@@ -1,13 +1,13 @@
 //
-//  Objc-Bridging-Header.h
 //  VrtcalSDK
 //
 //  Created by Scott McCoy on 7/3/23.
 //  Copyright © 2023 VRTCAL. All rights reserved.
 //
 
-#ifndef Objc_Bridging_Header_h
-#define Objc_Bridging_Header_h
+//@import OMIDVrtcalSDK
+
+//#import <OMSDK/OMIDImports.h>
 
 #import "OMIDAdEvents.h"
 #import "OMIDAdSession.h"
@@ -17,5 +17,3 @@
 #import "OMIDSDK.h"
 #import "OMIDScriptInjector.h"
 
-
-#endif /* Objc_Bridging_Header_h */
